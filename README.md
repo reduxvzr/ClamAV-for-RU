16b2bbf8d1c83a8cbbd8570ec41e887fde4d34f5
reduxvzr <romanch1819@gmail.com>
2024-08-15 00:52:46 +0300

    added script

---
+++ README.md
@@ -0,0 +1,1 @@
+Script for adding clamav databases in Russia
---
+++ clamav_change.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+systemctl stop clamav-freshclam
+rm -rf /var/lib/clamav/*
+wget http://unlix.ru/clamav/main.cvd -O /var/lib/clamav/main.cvd 
+wget http://unlix.ru/clamav/daily.cvd -O /var/lib/clamav/daily.cvd 
+wget http://unlix.ru/clamav/bytecode.cvd -O /var/lib/clamav/bytecode.cvd 
