7cef7fc15c1f4227693a9dbc71431c656ec4645d
reduxvzr <romancher@tutamail.com>
2024-10-14 20:58:24 +0300

    Update clamav_change.sh

--- clamav_change.sh
+++ clamav_change.sh
@@ -5,3 +5,4 @@ rm -rf /var/lib/clamav/*
 wget http://unlix.ru/clamav/main.cvd -O /var/lib/clamav/main.cvd 
 wget http://unlix.ru/clamav/daily.cvd -O /var/lib/clamav/daily.cvd 
 wget http://unlix.ru/clamav/bytecode.cvd -O /var/lib/clamav/bytecode.cvd 
+systemctl start clamav-freshclam
